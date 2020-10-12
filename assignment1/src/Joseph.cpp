cfd32260f013cedefe2010e0a9bfe1d4f9e82877
CYKS <370797515@qq.com>
2020-10-12 21:55:15 +0800

    更新main

--- assignment1/src/Joseph.cpp
+++ assignment1/src/Joseph.cpp
@@ -4,7 +4,7 @@
  * @Author: CYKS
  * @Date: 2020-10-06 21:27:01
  * @LastEditors: CYKS
- * @LastEditTime: 2020-10-11 21:49:09
+ * @LastEditTime: 2020-10-12 21:54:52
  */
 #include <iostream>
 #include <algorithm>
@@ -15,17 +15,17 @@ using namespace std;
 int main()
 {
 	int N_max, Y_max;
-	//cin >> N_max >> Y_max;
+	cin >> N_max >> Y_max;
 	freopen("output.out", "w", stdout);
     Link* link = new Link();
+	/*
 	for(int i = 1 ; i <= 1000; i++) {
 		cout << i << " " << link->solve(i, 5, 0) <<  endl;
 	}
-	/*
+	*/
 	for(int i = 1 ; i <= Y_max; i++) {
 		for(int j = 1 ; j <= N_max; j++) {
 			 cout << "(" << j << "," << i << "," << link->solve(j, i, 0) << ")" << endl;
 		}
 	}
-	*/
 }
