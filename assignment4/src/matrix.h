fac21a2335bceb66eca5d7b38db2a6fde20fc0aa
CYKS <370797515@qq.com>
2020-10-21 20:10:10 +0800

    添加assignment4

---
+++ assignment4/src/matrix.h
@@ -0,0 +1,34 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: CYKS
+ * @Date: 2020-10-21 19:15:33
+ * @LastEditors: CYKS
+ * @LastEditTime: 2020-10-21 19:19:37
+ */
+#include <iostream>
+
+#ifndef	MATRIX_H_
+
+#define MATRIX_H_
+
+class matrix
+{
+private:
+	/* data */
+public:
+	matrix(/* args */);
+	matrix operator + (const matrix &x) {
+		
+	}	
+
+	matrix operator * (const matrix &x) {
+
+	}
+
+	void insrt(int i, int j, int val) {
+		
+	}
+};
+
+#endif
