aa13f0bf832365b258c828485fa305eedb4dbc4d
MrPluto0 <710852553@qq.com>
2020-10-23 00:23:25 +0800

    main

---
+++ assignment4/src/Solution.h
@@ -0,0 +1,14 @@
+#include<iostream>
+
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+
+using namespace std;
+
+class Solution{
+    private:
+
+    public:
+};
+
+#endif
--- assignment4/src/matrix.h
+++ assignment4/src/matrix.h
@@ -4,7 +4,7 @@
  * @Author: CYKS
  * @Date: 2020-10-21 19:15:33
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-10-22 15:08:16
+ * @LastEditTime: 2020-10-22 22:51:35
  */
 #include <iostream>
 
@@ -12,43 +12,37 @@
 
 #define MATRIX_H_
 
-typedef struct tuple{
+using namespace std;
+
+typedef struct sparmattp{
 	//i,j为非零结点的横标，纵标
 	int i,j;
-	int num;
-}*tuple;
+	int data;
+	struct sparmattp* down;
+	struct sparmattp* right;
+}*sparmattp;
 
 class Matrix
 {
-private:
-	//m,n表示矩阵的行与列数，p表示稀疏矩阵中非零结点的个数
-	int m,n,p;
-	tuple* tuples;
-public:
-	Matrix(int **a,int rows,int cols){
-		m=rows;
-		n=cols;
-		tuples=new tuple[m*n];
-		int log=0;
-		for(int i=0;i<m;i++)
-			for(int j=0;j<n;j++)
-				if(a[i][j]!=INT_MAX){
-					tuples[log]->i=i+1;
-					tuples[log]->j=j+1;
-					tuples[log]->num=a[i][j];
-				}
-	}
-	Matrix operator + (const Matrix &x) {
+	private:
+		//m,n表示矩阵的行与列数，p表示稀疏矩阵中非零结点的个数
+		int m,n,p;
+		sparmattp sparse;
+	public:
+		Matrix(int **a,int rows,int cols){
+	
+		}
+		Matrix operator + (const Matrix &x) {
 		
-	}	
+		}	
 
-	Matrix operator * (const Matrix &x) {
+		Matrix operator * (const Matrix &x) {
 
-	}
+		}	
 
-	void insert(int i, int j, int val) {
+		void insert(int i, int j, int val) {
 		
-	}
+		}
 };
 
 #endif
