6b2e325cc0e69e234bfcdec3b9710319ccf9b435
MrPluto0 <710852553@qq.com>
2020-10-22 15:18:54 +0800

    main

--- assignment4/src/matrix.h
+++ assignment4/src/matrix.h
@@ -3,8 +3,8 @@
  * @version: 1.0.0
  * @Author: CYKS
  * @Date: 2020-10-21 19:15:33
- * @LastEditors: CYKS
- * @LastEditTime: 2020-10-21 19:19:37
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-10-22 15:08:16
  */
 #include <iostream>
 
@@ -12,21 +12,41 @@
 
 #define MATRIX_H_
 
-class matrix
+typedef struct tuple{
+	//i,j为非零结点的横标，纵标
+	int i,j;
+	int num;
+}*tuple;
+
+class Matrix
 {
 private:
-	/* data */
+	//m,n表示矩阵的行与列数，p表示稀疏矩阵中非零结点的个数
+	int m,n,p;
+	tuple* tuples;
 public:
-	matrix(/* args */);
-	matrix operator + (const matrix &x) {
+	Matrix(int **a,int rows,int cols){
+		m=rows;
+		n=cols;
+		tuples=new tuple[m*n];
+		int log=0;
+		for(int i=0;i<m;i++)
+			for(int j=0;j<n;j++)
+				if(a[i][j]!=INT_MAX){
+					tuples[log]->i=i+1;
+					tuples[log]->j=j+1;
+					tuples[log]->num=a[i][j];
+				}
+	}
+	Matrix operator + (const Matrix &x) {
 		
 	}	
 
-	matrix operator * (const matrix &x) {
+	Matrix operator * (const Matrix &x) {
 
 	}
 
-	void insrt(int i, int j, int val) {
+	void insert(int i, int j, int val) {
 		
 	}
 };
