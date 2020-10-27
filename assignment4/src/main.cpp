84abeb9af377ccee04e164d44f0f19c58ccedbb3
MrPluto0 <710852553@qq.com>
2020-10-27 16:48:57 +0800

    main

--- assignment4/src/main.cpp
+++ assignment4/src/main.cpp
@@ -3,8 +3,8 @@
  * @version: 1.0.0
  * @Author: CYKS
  * @Date: 2020-10-21 19:15:20
- * @LastEditors: CYKS
- * @LastEditTime: 2020-10-26 21:59:09
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-10-27 16:48:06
  */
 
 #include<iostream>
@@ -12,6 +12,14 @@
 using namespace std;
 
 int main(){
+    int input_code=0;
+    cout << "choose prefered mode, 0 for interactive, 1 for file input(input.in)" << endl;
+    cin>>input_code;
+    if(input_code==1){
+        //文件模式
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    }
     Solution slu;
     slu.readMatrix();
     slu.operate();
